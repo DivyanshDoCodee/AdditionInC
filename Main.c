d5b3f8feafb535a34cab7aa62ed5389ccf48ba68
DivyanshDoCodee <9922103268@mail.jiit.ac.in>
2024-07-24 11:23:06 +0530

    Create Main.c

---
+++ Main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int num1, num2,res;
+    
+    printf("Enter NUmber One : ");
+    scanf("%d",&num1);
+    
+    printf("Enter NUmber Two : ");
+    scanf("%d",&num2);
+    
+    res = num1 + num2;
+    
+    printf("The Result Is : %d ", res);
+}
